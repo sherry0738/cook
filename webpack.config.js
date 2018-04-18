3cf7f25b6b08ae30387b20d2cb8b6dcf8be63331
Sherry <lishuxian0738@gmail.com>
2018-04-18 17:36:11 +1000

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: "babel-loader"
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "eslint-loader"
       }
     ]
   }
